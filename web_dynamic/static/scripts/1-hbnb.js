62fe464916c4f35e0ce2147c7f1ac2442e4d99b2
kevin kibet <kevinkipkibet@gmail.com>
2022-10-30 16:32:17 +0300

    Added .gitignore

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 /* changes on each input checkbox tag */
 $(document).ready(
-function(){
-list = []
-$('#amenity_id').on('change', function(){
-list.append(this.id)
-})
-}
+    function () {
+        list_keys = {}
+        $("input[type='checkbox']").on('change', function () {
+            if ($(this).is(':checked')) {
+                list_keys[$(this).attr("data-id")] = $(this).attr("data-name");
+            } else {
+                delete list_keys[$(this).attr("data-id")];
+            }
+            if (list_keys != {}) {
+                $(".amenities h4").text(Object.values(list_keys));
+            }
+        })
+    }
 )
