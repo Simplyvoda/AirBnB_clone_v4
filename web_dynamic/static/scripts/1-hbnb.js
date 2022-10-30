a8e044acc2258a1893b4c7a772e391c1914aae39
Simplyvoda <efem.vodina@gmail.com>
2022-10-30 20:10:20 +0100

    commit

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 /* changes on each input checkbox tag */
+const $ = window.$;
 $(document).ready(
-    function () {
-        list_keys = {}
-        $("input[type='checkbox']").on('change', function () {
-            if ($(this).is(':checked')) {
-                list_keys[$(this).attr("data-id")] = $(this).attr("data-name");
-            } else {
-                delete list_keys[$(this).attr("data-id")];
-            }
-            if (list_keys != {}) {
-                $(".amenities h4").text(Object.values(list_keys));
-            }
-        })
-    }
-)
+  function () {
+    const listKeys = {};
+    $("input[type='checkbox']").on('change', function () {
+      if ($(this).is(':checked')) {
+        listKeys[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete listKeys[$(this).attr('data-id')];
+      }
+      if (listKeys !== {}) {
+        $('.amenities h4').text(Object.values(listKeys));
+      }
+    });
+  }
+);
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 /* changes on each input checkbox tag */
+const $ = window.$;
 $(document).ready(function () {
-    $.ajax({
-      url: "http://127.0.0.1:5001/api/v1/status",
-        success: function (data) {
-            if (data.status === 'OK') {
-            
-                $("div#api_status").addClass("available");
-            } else {
-                
-                $("div#api_status").removeClass("available");
-            }
-        }
-    });
+  $.ajax({
+    url: 'http://127.0.0.1:5001/api/v1/status',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    }
+  });
 });
